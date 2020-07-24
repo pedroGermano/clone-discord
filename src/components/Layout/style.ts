629db358ae0cdc03054c5520ffbbe845667b132c
Pedro Germano <pedrogermano232@gmail.com>
2020-07-24 06:52:12 -0300

    first commit

---
+++ src/components/Layout/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const Grid = styled.div`
+  display:  grid;
+
+  grid-auto-columns: 71px 240px auto 240px;
+  grid-template-rows: 46px auto 52px;
+  grid-template-areas: 
+    'SL SN CI CI'
+    'SL CL CD UL'
+    'SL UI CD UL';
+
+    height: 100vh;
+`;
+
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Container, Title, ExpandIcon } from './style'
+
+
+
+const ServerName: React.FC = () => {
+    return (
+        <Container>
+            <Title>Servidor do Pedro</Title>
+
+            <ExpandIcon />
+        </Container>
+    )
+}
+
+export default ServerName
